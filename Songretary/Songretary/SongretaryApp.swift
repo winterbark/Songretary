f57693e5714db9e400348bd77fba5fe1990c73ea
Darian Silvers <60489861+winterbark@users.noreply.github.com>
2023-02-07 11:21:01 -0500

    Add files via upload

---
+++ Songretary/Songretary/SongretaryApp.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+@main
+struct SongretaryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Songretary/Songretary/Views/FeatureRow.swift
@@ -0,0 +1,28 @@
+
+
+import SwiftUI
+
+struct FeatureRow: View {
+    var feature: Feature
+    
+    var body: some View {
+        HStack {
+            feature.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(feature.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct featureRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            FeatureRow(feature: features[0])
+            FeatureRow(feature: features[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
